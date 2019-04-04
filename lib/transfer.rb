b274e4a4bef240d35f43e88a4de1e602954f3767
Adam Kearney <aklives@hotmail.com>
2019-04-04 19:32:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,12 +21,12 @@ class Transfer
   end 
 
   def execute_transaction
-    if @status != "complete" && @sender.valid? == true
+    if @status != "complete" && @sender.valid? 
       @sender.balance -= amount
       @receiver.balance += amount
       @status = "complete"
     else
-      
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
